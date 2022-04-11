e2912aab51a97fb2cada690081558f56e0da24a6
Leandro <73486095+leandro144@users.noreply.github.com>
2022-04-11 16:23:48 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date (2022, 3, 5), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date (2022, 3, 5), category: 'food', title: 'Burguer King', value: 28 },
+    { date: new Date (2022, 3, 8), category: 'Despesa', title: 'Aluguel Apt', value: 2300 },
+    { date: new Date (2022, 4, 10), category: 'salary', title: 'Salario ACME', value: 4500 },
+];
